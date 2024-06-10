8e02f42edffc540af556bfdcfbea892b71a1b473
Huynh Minh Thuan <hmtminhthuan@gmail.com>
2024-06-10 14:04:30 +0700

    Update

--- src/app/(management)/users/components/UserProvider.tsx
+++ src/app/(management)/users/components/UserProvider.tsx
@@ -4,7 +4,6 @@ import { AuthProvider } from "@/contexts/JWTContext";
 import { AppProvider } from "@/contexts/AppContext";
 import AuthGuard from "@/guards/AuthGuard";
 import UserList from "./UserComponent";
-import useProtectData from "@/hooks/useProtectData";
 
 const UserListComponent = (props: {}) => {
   return (
